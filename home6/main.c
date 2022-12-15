33931b0b79dbc5ef691fe6f77f1c1c7210abbe38
Sergey Baykov <baykov-sv@yandex.ru>
2022-12-15 12:36:54 +0100

    home6 part1 ready

--- home6/main.c
+++ home6/main.c
@@ -4,12 +4,15 @@
 
 int main(void)
 {
-	double a = 1.0, b, c = 3.0, y = 5.0, sa, sb, x0, e = 1, err = 1e-7, summ;
+	double a = 1.0, b, c = 3.0, y = 5.0, sa, sb, x0, e = 1, err1 = 1e-7;
+	double err2 = 1e-7, summ;
+
+	// Часть 1
 	b = (c + a) / 2.0;
 
-	while(fabs(e) > err)
+	while(fabs(e) > err1)
 	{
-		printf("a=%.7lf b=%.7lf c=%.7lf ", a, b, c);
+		//printf("a=%.7lf b=%.7lf c=%.7lf ", a, b, c);
 
 		sa = sinh(a);
 		sb = sinh(b);
@@ -24,14 +27,16 @@ int main(void)
 		e = x0 - b;
 
 		x0 = b;
-		printf("x0=%.7lf e=%.7lf\n", x0, e);
+		//printf("x0=%.7lf e=%.7lf\n", x0, e);
 
 	}
 
-	
+	// Часть 2
+
+
 
 
 	printf("%.7lf\n", x0);
-	printf("%.6lf\n", err);
+	printf("%.6lf\n", summ);
 	return 0;
 }
